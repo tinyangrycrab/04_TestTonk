86eef62c096dc42b6b7d322b52a51f80851c4d2a
Luke <lukusridley@hotmail.com>
2016-08-21 15:57:07 -0700

    sicknazty  neon shit

---
+++ TestTonk/Source/TestTonk/TestTonk.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TestTonk.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestTonk, "TestTonk" );
